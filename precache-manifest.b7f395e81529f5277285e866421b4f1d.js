17eece0287e67b8cad63688db774fd17137317d5
Harshit <harshitupadhyay872@gmail.com>
2023-03-31 23:26:13 +0530

    Updates

---
+++ precache-manifest.b7f395e81529f5277285e866421b4f1d.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "3d7e2c93c10e0403f7f3",
+    "url": "/diamond-addy/static/css/main.a68515c2.chunk.css"
+  },
+  {
+    "revision": "3d7e2c93c10e0403f7f3",
+    "url": "/diamond-addy/static/js/main.3d7e2c93.chunk.js"
+  },
+  {
+    "revision": "e459cc6ec89340cad24c",
+    "url": "/diamond-addy/static/js/1.e459cc6e.chunk.js"
+  },
+  {
+    "revision": "70c93d0444a87101285e",
+    "url": "/diamond-addy/static/js/2.70c93d04.chunk.js"
+  },
+  {
+    "revision": "049895bfcdc0620d85b0",
+    "url": "/diamond-addy/static/js/runtime~main.049895bf.js"
+  },
+  {
+    "revision": "5829eb34f4a36b233d8cbf0f0a5c36df",
+    "url": "/diamond-addy/static/media/login.5829eb34.jpg"
+  },
+  {
+    "revision": "1e988f52fdb27d9443d843cd8eae6b82",
+    "url": "/diamond-addy/static/media/register.1e988f52.jpg"
+  },
+  {
+    "revision": "a4c88ad765b83a4cc3fa2c3b59aa1867",
+    "url": "/diamond-addy/static/media/marquee.a4c88ad7.jpg"
+  },
+  {
+    "revision": "1893ef65f8b39d317948865af9fe9759",
+    "url": "/diamond-addy/static/media/refresh.1893ef65.png"
+  },
+  {
+    "revision": "544a920dd951239efb587b260e2907ae",
+    "url": "/diamond-addy/static/media/deposit.544a920d.png"
+  },
+  {
+    "revision": "ae9f8ea489ea9a69a58909f415fa99b5",
+    "url": "/diamond-addy/static/media/withdraw.ae9f8ea4.png"
+  },
+  {
+    "revision": "8047ba09324761bbfc792f3c0a98060e",
+    "url": "/diamond-addy/static/media/logo.8047ba09.png"
+  },
+  {
+    "revision": "e85838a4710f57a7dbb3a3c158b95c4c",
+    "url": "/diamond-addy/static/media/flag.e85838a4.png"
+  },
+  {
+    "revision": "ae0bfa82f983cf1832c7038bac0fb9c5",
+    "url": "/diamond-addy/static/media/auto-ewallet.ae0bfa82.jpg"
+  },
+  {
+    "revision": "a2465140fa88edf31673a01e47788710",
+    "url": "/diamond-addy/static/media/online.a2465140.jpg"
+  },
+  {
+    "revision": "4b0662c4c4bd847e9aca903917f387fa",
+    "url": "/diamond-addy/static/media/ATM.4b0662c4.jpg"
+  },
+  {
+    "revision": "d3e751242736f24f5d08556d59c8c6a8",
+    "url": "/diamond-addy/index.html"
+  }
+];
